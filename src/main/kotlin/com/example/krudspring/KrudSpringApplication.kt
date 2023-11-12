b3a552526b5d9846e4ef2f3d33228532db39e24c
Jelle van der Zwaag <vanderzwaag@neozo.de>
2023-11-12 17:21:39 +0100

    Init commit

---
+++ src/main/kotlin/com/example/krudspring/KrudSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.krudspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KrudSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KrudSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/krudspring/KrudSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.krudspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KrudSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
