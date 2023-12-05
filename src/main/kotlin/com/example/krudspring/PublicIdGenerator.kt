e9a9f309c407fc548d792143acd34828b1742765
Jelle van der Zwaag <vanderzwaag@neozo.de>
2023-12-05 10:13:10 +0100

    Commit

--- src/main/kotlin/com/example/krudspring/PublicIdGenerator.kt
+++ src/main/kotlin/com/example/krudspring/PublicIdGenerator.kt
@@ -1,5 +1,13 @@
 package com.example.krudspring
 
+import org.springframework.stereotype.Component
+import java.util.*
+
 interface PublicIdGenerator {
     fun supply(): String
+}
+
+@Component
+class UUIDGenerator : PublicIdGenerator {
+    override fun supply() = UUID.randomUUID().toString()
 }
