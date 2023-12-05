c817fcd67398a6a41006d73397a91fda8c347593
Jelle van der Zwaag <vanderzwaag@neozo.de>
2023-12-05 08:31:59 +0100

    Commit

--- src/main/kotlin/com/example/krudspring/User.kt
+++ src/main/kotlin/com/example/krudspring/User.kt
@@ -3,31 +3,10 @@ package com.example.krudspring
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
+@Table("user")
 data class User(
 
     @Id
     val publicId: String? = null,
     val name: String,
 )
-
-@Table("user")
-data class NewUser (
-    override val name: String,
-): User2 {
-    override val publicId: String?
-        get() = null
-}
-
-@Table("user")
-data class ExistingUser (
-    override val name: String,
-    override val publicId: String,
-): User2
-
-
-sealed interface User2 {
-    val name: String
-    @get:Id
-    val publicId: String?
-}
-
